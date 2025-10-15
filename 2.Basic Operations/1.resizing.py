fe42d8da6db3cfbad5b485d6c1c0e58282a06fe9
NayeemHossenJim <nayeemhossenjim@gmail.com>
2025-10-15 14:29:19 +0600

    Image resize and crop

---
+++ 2.Basic Operations/1.resizing.py
@@ -0,0 +1,12 @@
+import os
+import cv2
+
+img = cv2.imread('Cow.jpg')
+resized_img = cv2.resize(img, (640, 640))
+
+print(img.shape)
+print(resized_img.shape)
+
+cv2.imshow('img', img)
+cv2.imshow('resized_img', resized_img)
+cv2.waitKey(0)
