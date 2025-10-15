fd082fdf8ce92f8fb9ad2031fe33e2ed4779993e
NayeemHossenJim <nayeemhossenjim@gmail.com>
2025-10-15 12:37:46 +0600

    IO Video

---
+++ 1.io_code/2.io_vid.py
@@ -0,0 +1,17 @@
+import os
+import cv2
+
+# read video
+video = cv2.VideoCapture("Cow.mp4")
+
+# visualize video
+ret = True
+while ret:
+    ret, frame = video.read()
+
+    if ret:
+        cv2.imshow('frame', frame)
+        cv2.waitKey(14)
+
+video.release()
+cv2.destroyAllWindows()
