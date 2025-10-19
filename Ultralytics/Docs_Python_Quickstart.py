c4132225f38e843199d56acf1280e2c811edfee3
NayeemHossenJim <nayeemhossenjim@gmail.com>
2025-10-19 10:04:47 +0600

    Complete quickstart

--- Ultralytics/Docs_Python_Quickstart.py
+++ Ultralytics/Docs_Python_Quickstart.py
@@ -1,8 +1,5 @@
 from ultralytics import YOLO
 
-model = YOLO("C:/Computer-Vision/Ultralytics/yolo12n.pt")
-
-results = model.predict("C:/Computer-Vision/data/Walking.mp4")
-
-# Export the model to ONNX format
-success = model.export(format="onnx")
+model = YOLO("C:/Computer-Vision/Ultralytics/yolo12n.pt") 
+results = model.train(epochs=5)
+model.val()
